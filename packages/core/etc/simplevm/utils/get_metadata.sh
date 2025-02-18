30be6dd164450e8cb8c58f9c655c5983cea265b7
vktrrdk <accounts@viktorrudko.de>
2025-02-18 13:41:51 +0000

    Extend timeframe from timer

--- packages/core/etc/simplevm/utils/get_metadata.sh
+++ packages/core/etc/simplevm/utils/get_metadata.sh
@@ -6,6 +6,12 @@ METADATA_OUTPUT_FILE="/etc/simplevm/metadata.json"
 /etc/simplevm/utils/rotate_logs.sh
 /etc/simplevm/utils/update_compatibilities.sh
 
+
+current_interval=$(grep '^OnUnitActiveSec=' $TIMER_PATH | cut -d= -f2 | sed 's/min//')
+default_interval="5min"
+max_interval="30min"
+TIMER_PATH="/etc/systemd/system/simplevm-metadata-synchronizer.timer"
+
 source /etc/simplevm/metadata_config.env
 
 log_message() {
@@ -27,12 +33,14 @@ if [ -z "${REAL_METADATA_ENDPOINT}" ]; then
 
   if [ $? -ne 0 ]; then
     log_message "Error: Failed to fetch metadata endpoint"
-    exit 1
+    extend_interval
+    exit 1  
   fi
 
   # Validate the JSON response
   if ! jq -e '.metadata_endpoint' <<< "${info_response}" &> /dev/null; then
     log_message "Error: Invalid JSON response from metadata endpoint"
+    extend_interval
     exit 1
   fi
 
@@ -49,6 +57,7 @@ metadata_response=$(curl -s -X GET "${REAL_METADATA_ENDPOINT}/metadata/${LOCAL_I
 
 if [ $? -ne 0 ]; then
   log_message "Error: Failed to fetch metadata"
+  extend_interval
   exit 1
 fi
 
@@ -59,6 +68,33 @@ echo "${metadata_response}"| jq > "${TMP_FILE}"
 # Rename the temporary file to the original filename
 if mv -f "${TMP_FILE}" "${METADATA_OUTPUT_FILE}"; then
   log_message "Metadata saved to ${METADATA_OUTPUT_FILE}"
+  reset_interval
 else
+  extend_interval
   log_message "Error: Failed to save metadata to ${METADATA_OUTPUT_FILE}"
 fi
+
+set_new_interval() {
+    # Update the timer file
+    sudo sed -i "s/^OnUnitActiveSec=.*/OnUnitActiveSec=$1/" $TIMER_PATH
+    # Reload systemd to apply changes
+    sudo systemctl daemon-reload
+    # Restart the timer with the new interval
+    sudo systemctl restart simplevm-metadata-synchronizer.timer
+}
+
+extend_interval() {
+    # Calculate the new interval
+    new_interval=$((current_interval + 5))
+    # Ensure we do not exceed the max interval
+    if (( new_interval > MAX_INTERVAL )); then
+        new_interval=$MAX_INTERVAL
+    fi
+    echo "Extending timer interval to ${new_interval} minutes"
+    set_new_interval "${new_interval}min"
+}
+
+reset_interval() {
+    echo "Resetting timer interval to default: $DEFAULT_INTERVAL"
+    set_new_interval "$DEFAULT_INTERVAL"
+}
