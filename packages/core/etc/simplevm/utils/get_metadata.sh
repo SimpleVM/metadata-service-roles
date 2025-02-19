ab7b721c23539bbd249a235772fe2b2983b741ce
vktrrdk <accounts@viktorrudko.de>
2025-02-19 15:45:09 +0000

    working on adjustments

--- packages/core/etc/simplevm/utils/get_metadata.sh
+++ packages/core/etc/simplevm/utils/get_metadata.sh
@@ -3,20 +3,51 @@
 LOG_FILE="/var/log/metadata.log"
 METADATA_OUTPUT_FILE="/etc/simplevm/metadata.json"
 
-/etc/simplevm/utils/rotate_logs.sh
-/etc/simplevm/utils/update_compatibilities.sh
-
-
-current_interval=$(grep '^OnUnitActiveSec=' $TIMER_PATH | cut -d= -f2 | sed 's/min//')
-default_interval="5min"
-max_interval="30min"
-TIMER_PATH="/etc/systemd/system/simplevm-metadata-synchronizer.timer"
-
-source /etc/simplevm/metadata_config.env
-
 log_message() {
   echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
+
+TIMER_PATH="/etc/systemd/system/simplevm-metadata-synchronizer.timer"
+
+DEFAULT_INTERVAL="5min"
+MAX_INTERVAL="30min"
+
+current_interval=$(grep '^OnUnitActiveSec=' $TIMER_PATH | cut -d= -f2 | sed 's/min//')
+log_message "This is my interval: ${current_interval}"
+
+
+set_new_interval() {
+    # Update the timer file
+    sudo sed -i "s/^OnUnitActiveSec=.*/OnUnitActiveSec=$1/" $TIMER_PATH
+    # Reload systemd to apply changes
+    sudo systemctl daemon-reload
+    # Restart the timer with the new interval
+    sudo systemctl restart simplevm-metadata-synchronizer.timer
+}
+
+extend_interval() {
+    # Calculate the new interval
+    new_interval=$((current_interval + 5))
+    # Ensure we do not exceed the max interval
+    if (( new_interval > MAX_INTERVAL )); then
+        new_interval=$MAX_INTERVAL
+    fi
+    echo "Extending timer interval to ${new_interval} minutes"
+    set_new_interval "${new_interval}min"
+}
+
+reset_interval() {
+    echo "Resetting timer interval to default: $DEFAULT_INTERVAL"
+    set_new_interval "$DEFAULT_INTERVAL"
+}
+
+/etc/simplevm/utils/rotate_logs.sh
+#/etc/simplevm/utils/update_compatibilities.sh
+
+
+
+source /etc/simplevm/metadata_config.env
+
 
 # Construct the URL with hostname query param
 INFO_ENDPOINT_URL="${METADATA_INFO_ENDPOINT}?hostname=$(hostname)"
@@ -26,6 +57,8 @@ AUTH_HEADER="auth_token: ${METADATA_ACCESS_TOKEN}"
 
 # Get real metadata endpoint from config if available
 REAL_METADATA_ENDPOINT=$(grep '^REAL_METADATA_ENDPOINT=' "/etc/simplevm/metadata_config.env" | cut -d '=' -f 2-)
+
+
 
 if [ -z "${REAL_METADATA_ENDPOINT}" ]; then
   # Fetch the JSON response from the info endpoint to get real metadata endpoint
@@ -52,7 +85,9 @@ if [ -z "${REAL_METADATA_ENDPOINT}" ]; then
 fi
 
 # Fetch the actual metadata from the extracted endpoint
-LOCAL_IP=$(hostname -I | awk '{print $1}')
+LOCAL_IP="192.168.2.37" # adjust for development purposes
+log_message ${LOCAL_IP}
+#LOCAL_IP=$(hostname -I | awk '{print $1}')
 metadata_response=$(curl -s -X GET "${REAL_METADATA_ENDPOINT}/metadata/${LOCAL_IP}" -H "${AUTH_HEADER}")
 
 if [ $? -ne 0 ]; then
@@ -73,28 +108,3 @@ else
   extend_interval
   log_message "Error: Failed to save metadata to ${METADATA_OUTPUT_FILE}"
 fi
-
-set_new_interval() {
-    # Update the timer file
-    sudo sed -i "s/^OnUnitActiveSec=.*/OnUnitActiveSec=$1/" $TIMER_PATH
-    # Reload systemd to apply changes
-    sudo systemctl daemon-reload
-    # Restart the timer with the new interval
-    sudo systemctl restart simplevm-metadata-synchronizer.timer
-}
-
-extend_interval() {
-    # Calculate the new interval
-    new_interval=$((current_interval + 5))
-    # Ensure we do not exceed the max interval
-    if (( new_interval > MAX_INTERVAL )); then
-        new_interval=$MAX_INTERVAL
-    fi
-    echo "Extending timer interval to ${new_interval} minutes"
-    set_new_interval "${new_interval}min"
-}
-
-reset_interval() {
-    echo "Resetting timer interval to default: $DEFAULT_INTERVAL"
-    set_new_interval "$DEFAULT_INTERVAL"
-}
